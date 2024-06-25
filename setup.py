971e22e872fdea247505f84485e94de968ac1b6a
Meredith Thomas <meredityman@gmail.com>
2024-06-25 16:54:21 +0200

    WC

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='timecrawler',
       version='0.0',
@@ -7,5 +7,21 @@ setup(name='timecrawler',
       author='Meredith Thomas',
       author_email='meredityman@gmail.com',
       license='MIT',
-      packages=['timecrawler'],
-      zip_safe=False)
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+            "certifi>=2024.2.2",
+            "charset-normalizer>=3.3.2",
+            "idna>=3.7",
+            "isodate>=0.6.1",
+            "pillow>=10.3.0",
+            "pyparsing>=3.1.2",
+            "rdflib>=7.0.0",
+            "requests>=2.32.2",
+            "six>=1.16.0",
+            "SPARQLWrapper>=2.0.0",
+            "urllib3>=2.2.1",
+            "huggingface_hub>=0.23.0",
+            # "llama_cpp_python>=0.2.79"
+      ]
+    )
