64ffb9335c77356ef410c76a5a9af96769ae7829
Meredith Thomas <meredityman@gmail.com>
2024-06-25 16:25:12 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='timecrawler',
+      version='0.0',
+      description='',
+      url='https://github.com/meredityman/timecrawler',
+      author='Meredith Thomas',
+      author_email='meredityman@gmail.com',
+      license='MIT',
+      packages=['winterfell'],
+      zip_safe=False)
